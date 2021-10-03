8016bfb41a740e2ec09f3d969c30106d5e434e3b
thavetta <43245361+thavetta@users.noreply.github.com>
2021-10-03 22:25:12 +0200

    Lab 8 úkol 1

---
+++ Laby/Lab8/Ukol 1/PouzitiOsob/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Osoby;
+
+namespace PouzitiOsob
+{
+    public static class Utils
+    {
+        public static int VratAktualnyVek(this Osoba osoba)
+        {
+            return DateTime.Now.Year - osoba.RokNarozeni;
+        }
+
+        public static void Add(this SeznamOsob seznam, Osoba osoba)
+        {
+            seznam.PridejOsobu(osoba);
+        }
+
+        //Metoda umožní přiřadit instanci Osoba do ValueTuple<int, string, string>
+        public static void Deconstruct(this Osoba osoba, out int id, out string prijmeni, out string jmeno )
+        {
+            id = osoba.ID;
+            jmeno = osoba.Jmeno;
+            prijmeni = osoba.Prijmeni;
+        }
+    }
+}
---
+++ Laby/Lab8/Ukol 1/README.md
@@ -0,0 +1,12 @@
+# Lab 8 - úkol 1
+## Osoby a C# 7
+* Zajistěte možnost vytvořit přiřazením z osoby tuple obsahující jméno, příjmení a id
+
+## Doporučený postup
+1. Zkopírujte složku s projektem Lab7 do složky Lab8
+1. Přejmenujte Solution file na Lab8.sln
+1. Doplňte do třídy Utils extenzní metodu Deconstruct třídě Osoba tak, aby vybrala a nastavila požadované hodnoty z instance (pomocí out parametrů metody Deconstruct)
+1. Přidejte do třídy SeznamOsob indexer, kterým umožníte přístup k prvkům kolekce pomocí indexu
+1. Otestujte na nultém prvku kolekce přiřazení osoby do ValueTuple s využitím podpory v jazyce C#
+1. Použijte ve výpisu získané hodnoty
+1. Otestujte funkčnost řešení
